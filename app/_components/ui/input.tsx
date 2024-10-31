fd1d77d47b3548387148f89ae7e6b22cd84ef85d
Caio <caiobarreto1acim@gmail.com>
2024-10-31 16:37:39 -0300

    feat: add a refatored input

--- app/_components/ui/input.tsx
+++ app/_components/ui/input.tsx
@@ -1,11 +1,8 @@
 import * as React from "react"
-
 import { cn } from "@/app/_lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+// Remova a interface InputProps
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
